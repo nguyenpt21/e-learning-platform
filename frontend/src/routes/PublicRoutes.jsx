5ec19f1b284dde515711c6e21dcd76e99c4b5887
ngoquyen <reinekaiser@gmail.com>
2025-10-06 04:37:11 +0700

    add shadcn

--- frontend/src/routes/PublicRoutes.jsx
+++ frontend/src/routes/PublicRoutes.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "../pages/Student/HomePage";
+import CourseDetail from "../pages/Student/CourseDetail";
 
 
 const PublicRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-        
+            <Route path="/course/:_id" element={<CourseDetail/>} />
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     );
