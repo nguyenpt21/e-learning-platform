978e89b000ca6a938a4fe3c4b5d830fe7fe26c7f
ngoquyen <reinekaiser@gmail.com>
2026-01-18 12:20:55 +0700

    metadata for course

--- frontend/src/components/student/Layout.jsx
+++ frontend/src/components/student/Layout.jsx
@@ -3,6 +3,7 @@ import Header from "../Header"
 import Footer from "../Footer"
 import { Outlet } from 'react-router-dom';
 import MyCoursesLoader from '@/components/student/home-page/MyCourseLoader';
+import SEO from '@/components/SEO';
 
 const Layout = () => {
     return (
